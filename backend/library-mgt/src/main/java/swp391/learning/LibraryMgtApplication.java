b6cf93ab32a5e755849b0435312fd19a6efd0052
Dien Nguyen <ndien9203@gmail.com>
2024-05-25 14:50:57 +0700

    Initial commit

---
+++ backend/library-mgt/src/main/java/swp391/learning/LibraryMgtApplication.java
@@ -0,0 +1,13 @@
+package swp391.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMgtApplication.class, args);
+	}
+
+}
---
+++ backend/library-mgt/src/test/java/swp391/learning/LibraryMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package swp391.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
