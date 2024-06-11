1f6af03950c99634893b922214eda813234c02b7
phucnhhe181045 <phucnhhe181045@fpt.edu.vn>
2024-06-11 22:31:37 +0700

    complete call api for login and register

--- frontend/src/page/Register.jsx
+++ frontend/src/page/Register.jsx
@@ -3,18 +3,16 @@ import { Form, Button, Alert } from "react-bootstrap";
 import "../assets/style/Register.css";
 import Logo from "../assets/image/logo.png";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import authApi from '../api/AuthAPI';
 
-const SignUp = () => {
+const Register = () => {
   const [user, setUser] = useState({
     fullName: '',
     email: '',
     password: '',
-    confirmPassword: '',
     phone: '',
-    username: '',
     gender: '',
     dob: new Date(),
   });
@@ -32,18 +30,21 @@ const SignUp = () => {
   };
 
   const isValidInput = (data) => {
-    const { username, password, email, phone, fullName } = data;
+    const { password, email, phone, fullName, dob } = data;
     const specialCharRegex = /[!@#$%^&*(),.?":{}|<>0-9]/;
     const spaceRegex = /\s/;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const phoneRegex = /^\d{11}$/;
+    const phoneRegex = /^\d{10,11}$/;
 
-    if (spaceRegex.test(password) || spaceRegex.test(username) || spaceRegex.test(email) || spaceRegex.test(phone)) {
-      setErrorMessage('No spaces are allowed in password, username, email, and phone.');
+    const currentDate = new Date();
+    const minDob = new Date(currentDate.setFullYear(currentDate.getFullYear() - 16));
+
+    if (spaceRegex.test(password) || spaceRegex.test(email) || spaceRegex.test(phone)) {
+      setErrorMessage('No spaces are allowed in password, email, and phone.');
       return false;
     }
-    if (specialCharRegex.test(username) || specialCharRegex.test(fullName)) {
-      setErrorMessage('Username and Full Name cannot contain special characters or numbers.');
+    if (specialCharRegex.test(fullName)) {
+      setErrorMessage('Full Name cannot contain special characters or numbers.');
       return false;
     }
     if (!emailRegex.test(email)) {
@@ -51,39 +52,35 @@ const SignUp = () => {
       return false;
     }
     if (!phoneRegex.test(phone)) {
-      setErrorMessage('Phone number must be 11 digits.');
+      setErrorMessage('Phone number must be 10 or 11 digits.');
+      return false;
+    }
+    if (dob > minDob) {
+      setErrorMessage('You must be at least 16 years old to register.');
       return false;
     }
     return true;
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // if (user.password !== user.confirmPassword) {
-    //   setErrorMessage("Passwords do not match");
-    //   return;
-    // }
-
     if (!isValidInput(user)) return;
-
     try {
-      await axios.post('/api/v1/users/register', {
-        email: user.email,
-        password: user.password,
-        phone: user.phone,
-        fullName: user.fullName,
-        // username: user.username,
-        gender: user.gender,
-        dob: user.dob.toLocaleDateString('en-GB'),
+      authApi
+      .register(user)
+      .then((response) => {
+        console.log(response);
+        if (response.code === 200) {
+          localStorage.setItem('emailSignUp', user.email);
+          navigate('/verifyOtp');
+        } else {
+          setErrorMessage(response.message);
+          return window.alert(`Sign Up Error: ${response.data.message}`);
+        }
+      })
+      .catch((error) => {
+        return window.alert('Sign Up Error: System Error');
       });
-
-      const response = await axios.post('/', {
-        username: user.email,
-        password: user.password
-      });
-
-      const token = response.data.jwt;
-      localStorage.setItem('token', token);
-      navigate('/verify');
     } catch (error) {
       console.error("There was an error registering the user!", error);
       setErrorMessage("Registration failed. Please try again.");
@@ -99,7 +96,7 @@ const SignUp = () => {
         <div className="register-title">
           <h6>Registration</h6>
           <div className="sub-title">
-            Do have an account? <Link to="/login">Login here</Link>
+            Do you have an account? <Link to="/login">Login here</Link>
           </div>
         </div>
         {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
@@ -118,18 +115,6 @@ const SignUp = () => {
                   required
                 />
               </Form.Group>
-              {/* <Form.Group className="mb-3">
-                <Form.Label className="label">Username</Form.Label>
-                <Form.Control
-                  className="field-input"
-                  type="text"
-                  placeholder="Username"
-                  name="username"
-                  value={user.username}
-                  onChange={handleChange}
-                  required
-                />
-              </Form.Group> */}
               <Form.Group className="mb-3">
                 <Form.Label className="label">Email</Form.Label>
                 <Form.Control
@@ -168,7 +153,7 @@ const SignUp = () => {
                   required
                 />
               </Form.Group>
-              <Form.Group className="mb-4" >
+              <Form.Group className="mb-4">
                 <Form.Label className="label">Gender</Form.Label>
                 <Form.Control
                   as="select"
@@ -181,10 +166,10 @@ const SignUp = () => {
                   <option value="">Select Gender</option>
                   <option value="Male">Male</option>
                   <option value="Female">Female</option>
-                  </Form.Control>
+                </Form.Control>
               </Form.Group>
               <Form.Group className="mb-3 date-picker-container">
-                <Form.Label className="label" style={{ fontSize: 'small',fontWeight: 'bold', marginRight: '10px'}}>Date of Birth</Form.Label>
+                <Form.Label className="label" style={{ fontSize: 'small', fontWeight: 'bold', marginRight: '10px' }}>Date of Birth</Form.Label>
                 <DatePicker
                   selected={user.dob}
                   onChange={handleDateChange}
@@ -199,11 +184,7 @@ const SignUp = () => {
               </Form.Group>
             </div>
           </div>
-          <Button className="btn-register"  style={{
-                backgroundColor: "#F87555",
-                border: "none",
-                fontSize: "small",
-              }} type="submit">
+          <Button className="btn-register" style={{ backgroundColor: "#F87555", border: "none", fontSize: "small" }} type="submit">
             Register
           </Button>
         </Form>
@@ -212,4 +193,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Register;
